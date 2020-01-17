f817423d658665d650f25d3dc3c4a5f12697162b
kimiinglet <kinglet91391@gmail.com>
2020-01-17 16:58:13 -0600

    added some stuff

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const fs = require("fs");
 const inquirer = require('inquirer');
 const axios = require('axios');
 const generateHTML = require('./generateHTML');
-const pdf = require('html-pdf');
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile); // gives format to return promise
-const puppeteer = require('puppeteer');
 
 
-//prompt questions
+
+//prompt user for username and favorite color
 inquirer.prompt([
     {
         type: "input",
@@ -26,28 +25,57 @@ inquirer.prompt([
             "pink",
             "red"
         ]
-    },
-]).then(async function (data) {
-    let html = generateHtml(data);
-    return writeFileAsync("index.html", html);
-} ).then(function(){
-    console.log("Success!")
+    }
+])
+.then(function(res) {
+    const userName = res.username;
+    const userColor = res.color;
+
+    const queryURL = `https://api.github.com/users/${userName}`;
+    const starredURL = `https://api.github.com/users/${userName}/starred{/owner}{/repo}`;
+
+
+    //calling a function
+gitHubRequest(queryURL).then(function(){})
 });
 
-        // let gitHubData = await githubURL(userInput.username)
-        // console.log(githubURL);
+function gitHubRequest(queryURL) {
+    return axios.get(queryURL)
+    .then(function(gitResponse){
+        //avitar URL
+        const proPic = gitResponse.data.avatar_url + ".png";
 
+        //github Username
+        const gitUsername = gitResponse.data.login;
+        console.log(gitUsername);
 
-    // use user input data to use axios to request github stats and save as a variable
-    // let gitHubRes = axios request to appropriate url
-    // construct new object that contains github response and user input together and then send to generateHTML where you can then dynamically render the information
-    // let combinedData = {
-        // githubData: gitHubRes,
-        // userInputData: data
-    // }
-    // console.log(generateHTML(combinedData));
+        //user location
+        const location = gitResponse.data.location;
+        console.log(location);
 
-// function writeToFile(fileName, data) {
-// }
-// function init() {
-// }
+        //profile
+        const profileURL = gitResponse.data.html_url;
+        console.log(profileURL);
+
+        //blog
+        const blog = gitResponse.data.blog;
+        console.log(blog);
+
+        //bio
+        const userBio = gitResponse.data.bio;
+        console.log(userBio);
+
+        //public repositories
+        const publicRepos = gitResponse.data.public_repos;
+        console.log(publicRepos);
+
+        //numberOfFollowers
+        const followers = gitResponse.data.followers;
+        console.log(followers);
+
+        //numberOfFollowing
+        const following = gitResponse.data.following;
+        console.log(following)
+        
+    });
+};
